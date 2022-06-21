f6c646ea910787697c0f180b46e56141b223dfd8
Santech <Adamsanni74@gmail.com>
2022-06-21 21:36:24 +0100

    new form

--- commission.js
+++ commission.js
@@ -1,7 +1,72 @@
-let print = document.getElementById('message');
-function sa (){
-    print.innerHTML= 'Your Requested has been submitted for Aproval';
-}
-form.addEventListener=('Submit',sa)
 
-alert('ok')
+
+function logSubmit(event) {
+    log.textContent = `Your Request has been submitted for aproval!`;
+    event.preventDefault();
+  }
+  function MyFunction() {
+    var first;
+    first = document.getElementById("firstName").value;}
+
+
+  const name = document.getElementById('firstName')
+  const form = document.getElementById('form');
+  const log = document.getElementById('log');
+  form.addEventListener('submit', logSubmit);
+  
+
+
+
+
+
+
+
+
+
+
+
+
+{
+    "Sheet1" [
+        {
+            "Rack Name": "SRA 10",
+            "Rack Label": "SRA 10-8",
+            "Device Name": "Hp DL380Gen3",
+            "Serial Number": "CZJ544002fN",
+            "Host Name": "Hq-cicserv",
+            "Power Module": "Feed A + Feed B"
+        },
+        {
+            "Rack Name": "SRA 10",
+            "Rack Label": "SRA 10-5",
+            "Device Name": "Hp DL380Gen6",
+            "Serial Number": "CZJ144002DN",
+            "Host Name": "Hq-cicserv",
+            "Power Module": "Feed A + Feed B"
+        },
+        {
+            "Rack Name": "SRA 10",
+            "Rack Label": "SRA 10-12",
+            "Device Name": "Hp DL380Gen6",
+            "Serial Number": "CZJ547002DN",
+            "Host Name": "Hq-cicserv",
+            "Power Module": "Feed A + Feed B"
+        },
+        {
+            "Rack Name": "SRA 10",
+            "Rack Label": "SRA 10-11",
+            "Device Name": "Hp DL380Gen6",
+            "Serial Number": "CZJ554002DN",
+            "Host Name": "Hq-cicserv",
+            "Power Module": "Feed A + Feed B"
+        },
+        {
+            "Rack Name": "SRA 10",
+            "Rack Label": "SRA 10-2",
+            "Device Name": "Hp DL380Gen6",
+            "Serial Number": "CZJ544012DN",
+            "Host Name": "Hq-cicserv",
+            "Power Module": "Feed A + Feed B"
+        }
+    ]
+}
